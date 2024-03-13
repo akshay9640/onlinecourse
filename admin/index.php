4cf11271b2fd372d8997391735ae8f1238955d6f
Subrahmanyam Maradani <noreply@gramtarang.org>
2024-03-14 00:20:43 +0530

    Added Code

---
+++ admin/index.php
@@ -0,0 +1,104 @@
+<?php
+session_start();
+error_reporting(0);
+include("includes/config.php");
+if(isset($_POST['submit']))
+{
+    $username=$_POST['username'];
+    $password=md5($_POST['password']);
+$query=mysqli_query($con,"SELECT * FROM admin WHERE username='$username' and password='$password'");
+$num=mysqli_fetch_array($query);
+if($num>0)
+{
+$extra="change-password.php";//
+$_SESSION['alogin']=$_POST['username'];
+$_SESSION['id']=$num['id'];
+$host=$_SERVER['HTTP_HOST'];
+$uri=rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
+header("location:http://$host$uri/$extra");
+exit();
+}
+else
+{
+$_SESSION['errmsg']="Invalid username or password";
+$extra="index.php";
+$host  = $_SERVER['HTTP_HOST'];
+$uri  = rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
+header("location:http://$host$uri/$extra");
+exit();
+}
+}
+?>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+
+    <title>Admin Login</title>
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    <link href="assets/css/style.css" rel="stylesheet" />
+</head>
+<body>
+    <?php include('includes/header.php');?>
+    <div class="content-wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h4 class="page-head-line">Please Enter Login Credentials </h4>
+
+                </div>
+
+            </div>
+             <span style="color:red;" ><?php echo htmlentities($_SESSION['errmsg']); ?><?php echo htmlentities($_SESSION['errmsg']="");?></span>
+            <form name="admin" method="post">
+            <div class="row">
+                <div class="col-md-6">
+                     <label>Enter Username : </label>
+                        <input type="text" name="username" class="form-control" required />
+                        <label>Enter Password :  </label>
+                        <input type="password" name="password" class="form-control" required />
+                        <hr />
+                        <button type="submit" name="submit" class="btn btn-info"><span class="glyphicon glyphicon-user"></span> &nbsp;Log Me In </button>&nbsp;
+                </div>
+                </form>
+                <div class="col-md-6">
+                    <div class="alert alert-info">
+                        From the inception, Centurion Universitys DNA has been <b>Shaping lives and empowering communities.</b> The University has focused on hands-on, experience based, practice oriented learning that makes a difference through appropriate and relevant innovation and action research. While promoting Nano, Mini and Micro Enterprises,the University works toward learning experiences that are quantifiable, sustainable, scalable and replicable.
+                        <br />
+                         <strong> Centurion University is redefining the learning platform through::</strong>
+                        <ul>
+                            <li>
+                                Skill integrated, domain linked, teaching and learning
+                            </li>
+                            <li>
+                                Industry and community partnerships
+                            </li>
+                            <li>
+                                Creating and co-creating enterprises
+                            </li>
+                            <li>
+                                An eco-system approach which includes community, enterprise, industry and educational institutes.
+                            </li>
+                        </ul>
+                       
+                    </div>
+                                    </div>
+
+            </div>
+        </div>
+    </div>
+    <!-- CONTENT-WRAPPER SECTION END-->
+    <?php include('includes/footer.php');?>
+    <!-- FOOTER SECTION END-->
+    <!-- JAVASCRIPT AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
+    <!-- CORE JQUERY SCRIPTS -->
+    <script src="assets/js/jquery-1.11.1.js"></script>
+    <!-- BOOTSTRAP SCRIPTS  -->
+    <script src="assets/js/bootstrap.js"></script>
+</body>
+</html>
---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    &copy; Online Course Registration | By : M.V.R.Subrahmannyam
+                </div>
+
+            </div>
+        </div>
+    </footer>
